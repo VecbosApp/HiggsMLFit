64714fa42466918286f55a813f7358466746acb1
emanuele <>
2009-05-13 17:49:01 +0000

    scripts to submit toy Monte Carlo in batch

---
+++ submitToys.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+./SubmitGenerate.pl -r ~/scratch0/higgs/CMSSW_2_1_17 -n 1000 -j 10 -q 1nh -p 0 -b 160 -x src/GenerateHWW2e.cc -d pccmsrm21.cern.ch:/cmsrm/pc21/emanuele/data/Higgs2.1.X/toys
