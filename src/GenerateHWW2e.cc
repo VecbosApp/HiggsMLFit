5aa3daf9f3dd981df777555d8f65a930f2a0fb18
emanuele <>
2009-06-09 16:44:44 +0000

    add part to do fit parameters systematics

--- src/GenerateHWW2e.cc
+++ src/GenerateHWW2e.cc
@@ -18,6 +18,11 @@ MLOptions GetDefaultOptions() {
   opts.addBoolOption("useMinPt",        "Use pT of the slowest leptons", kTRUE);
   opts.addBoolOption("useMET",          "Use MET",                       kTRUE);
   opts.addBoolOption("useDxyzEVT",      "Use jet impact parameters",     kTRUE);
+  // smearing options for PDFs systematics
+  opts.addBoolOption("smearDeltaPhi",  "apply Gaussian smearing to deltaphi parameters", kFALSE);
+  opts.addBoolOption("smearMinPt",     "apply Gaussian smearing to minPt parameters",    kFALSE);
+  opts.addBoolOption("smearMET",       "apply Gaussian smearing to MET parameters",      kTRUE);
+  opts.addBoolOption("smearEff",       "apply Gaussian smearing to jet bin fraction",    kFALSE);
 
   return opts;
 }
@@ -164,6 +169,7 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
   sprintf(configfilename, "toyconfig/toyHWW-finalstate%d-%dGeV.config",finalstate,higgsmass);
 
   theFit.initialize(configfilename);
+
   MLWjetGenerator theGenerator(theFit, "myFit");
 
   Int_t ngen =
@@ -176,6 +182,55 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
   RooArgSet genVars(theFit.getObsList(MLStrList("deltaPhi","MET","minPt","dxyEVT","dszEVT")));
   MLToyStudy theStudy(theGenerator, genVars, "E", "MTE", 0, theFit.getNoNormVars("myFit"));
   theStudy.addFit(*myPdf);
+
+  RooAbsPdf *myPdf2 = 0;
+
+  // smear group of correlated parameters for systematics
+  if(opts.getBoolVal("smearDeltaPhi")) {
+    MLStrList sig_deltaPhi_params("sig_deltaPhi_mean","sig_deltaPhi_sigma","sig_deltaPhi_alpha","sig_deltaPhi_alphaCB","sig_deltaPhi_nCB");
+    MLStrList WW_deltaPhi_params("WW_deltaPhi_mean","WW_deltaPhi_sigmaL","WW_deltaPhi_sigmaR","WW_deltaPhi_alphaL","WW_deltaPhi_alphaR");
+    MLStrList ttbar_deltaPhi_params("ttbar_deltaPhi_p1","ttbar_deltaPhi_p2");
+    MLStrList other_deltaPhi_params("other_deltaPhi_mean1","other_deltaPhi_sigma1","other_deltaPhi_mean2","other_deltaPhi_sigma2","other_deltaPhi_f1");
+    MLStrList deltaPhi_params(sig_deltaPhi_params);
+    deltaPhi_params.Add(WW_deltaPhi_params);
+    deltaPhi_params.Add(ttbar_deltaPhi_params);
+    deltaPhi_params.Add(other_deltaPhi_params);
+    theFit.smearConstantParameters(deltaPhi_params);
+    myPdf2 = theFit.buildModel("myFit");
+  }
+
+  if(opts.getBoolVal("smearMinPt")) {
+    MLStrList sig_minPt_params("sig_minPt_mean","sig_minPt_sigmaL","sig_minPt_sigmaR","sig_minPt_alphaL","sig_minPt_alphaR");
+    MLStrList WW_minPt_params("WW_minPt_mean","WW_minPt_sigmaL","WW_minPt_sigmaR","WW_minPt_alphaL","WW_minPt_alphaR");
+    MLStrList ttbar_minPt_params("ttbar_minPt_mean","ttbar_minPt_sigmaL","ttbar_minPt_sigmaR","ttbar_minPt_alphaL","ttbar_minPt_alphaR");
+    MLStrList other_minPt_params("other_minPt_mean","other_minPt_sigmaL","other_minPt_sigmaR","other_minPt_alphaL","other_minPt_alphaR");
+    MLStrList minPt_params(sig_minPt_params);
+    minPt_params.Add(WW_minPt_params);
+    minPt_params.Add(ttbar_minPt_params);
+    minPt_params.Add(other_minPt_params);
+    theFit.smearConstantParameters(minPt_params);
+    myPdf2 = theFit.buildModel("myFit");
+  }
+
+  if(opts.getBoolVal("smearMinPt")) {
+    MLStrList sig_MET_params("sig_MET_bin1","sig_MET_bin2","sig_MET_bin3","sig_MET_bin4","sig_MET_bin5","sig_MET_bin6","sig_MET_bin7");
+    MLStrList WW_MET_params("WW_MET_bin1","WW_MET_bin2","WW_MET_bin3","WW_MET_bin4","WW_MET_bin5","WW_MET_bin6","WW_MET_bin7");
+    MLStrList ttbar_MET_params("ttbar_MET_bin1","ttbar_MET_bin2","ttbar_MET_bin3","ttbar_MET_bin4","ttbar_MET_bin5","ttbar_MET_bin6","ttbar_MET_bin7");
+    MLStrList other_MET_params("other_MET_bin1","other_MET_bin2","other_MET_bin3","other_MET_bin4","other_MET_bin5","other_MET_bin6","other_MET_bin7");
+    MLStrList MET_params(sig_MET_params);
+    MET_params.Add(WW_MET_params);
+    MET_params.Add(ttbar_MET_params);
+    MET_params.Add(other_MET_params);
+    theFit.smearConstantParameters(MET_params);
+    myPdf2 = theFit.buildModel("myFit");
+  }
+
+  if(opts.getBoolVal("smearEff")) {
+    theFit.smearConstantParameters(MLStrList("eff_sig","eff_WW"));
+    myPdf2 = theFit.buildModel("myFit");
+  }
+
+  if( myPdf2 !=0 ) theStudy.addFit(*myPdf2);
 
   theStudy.generateAndFit(nexp,ngen);
 
